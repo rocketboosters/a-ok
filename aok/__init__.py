742b13250a4e825b3c426cef02779155a61cecb5
Scott Ernst <swernst@gmail.com>
2020-12-14 15:28:28 -0600

    Library Tweaks
    Update for easier library version support.

--- aok/__init__.py
+++ aok/__init__.py
@@ -1,2 +1,16 @@
+import pathlib as _pathlib
+from importlib import metadata as _metadata
+
+import toml as _toml
+
 from aok._operations import to_comparator  # noqa
 from aok._comparisons import *  # noqa
+
+try:
+    __version__ = _metadata.version(__package__)
+except _metadata.PackageNotFoundError:  # pragma: no-cover
+    # If the package is not installed such that it has distribution metadata
+    # fallback to loading the version from the pyproject.toml file.
+    __version__ = _toml.loads(
+        _pathlib.Path(__file__).parent.parent.joinpath("pyproject.toml").read_text()
+    )["tool"]["poetry"]["version"]
